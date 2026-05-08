69c5829457b304bf38d6815420abd87af13122a8
shivam <ssharma4@wpi.edu>
2026-05-08 09:31:45 +0530

    Create 137-SingleNumberIi

---
+++ 137-SingleNumberIi/137-SingleNumberIi.py
@@ -0,0 +1,11 @@
+# Last updated: 08/05/2026, 09:31:45
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones=0
+        twos=0
+
+        for num in nums:
+            ones= ones^num & ~(twos)
+            twos= twos^num & ~(ones)
+
+        return ones
