a67e65694694f46eb5bb6e8fbed9f50137d70dae
shivam <ssharma4@wpi.edu>
2026-05-08 09:27:23 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,13 @@
+// Last updated: 08/05/2026, 09:27:22
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
