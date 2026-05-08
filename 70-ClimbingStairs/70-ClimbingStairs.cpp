8ae8b0b95c231a3852dc3513c01545836e9b0ecd
shivam <ssharma4@wpi.edu>
2026-05-08 09:32:50 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,23 @@
+// Last updated: 08/05/2026, 09:32:49
+class Solution {
+public:
+
+    int climbStairs(int n, unordered_map<int,int>& memo) {
+
+        if(n==0 || n==1){
+        return 1;
+        }
+        if(memo.find(n)==memo.end()){
+            memo[n]= climbStairs(n-1, memo) + climbStairs(n-2,memo);
+        }
+
+        return memo[n];
+          
+    }
+
+    int climbStairs(int n){
+        unordered_map<int, int> memo;
+
+        return climbStairs(n, memo);
+    }
+};
