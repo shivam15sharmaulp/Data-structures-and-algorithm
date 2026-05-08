83583011065d9f6a78a43876056020cc118ef4d8
shivam <ssharma4@wpi.edu>
2026-05-08 09:31:22 +0530

    Create 155-MinStack

---
+++ 155-MinStack/155-MinStack.py
@@ -0,0 +1,36 @@
+# Last updated: 08/05/2026, 09:31:22
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        self.minstack=[]
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.minstack)>0:
+            if self.minstack[-1]<val:
+                self.minstack.append(self.minstack[-1])
+            else:
+                self.minstack.append(val)
+        else:
+            self.minstack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        # print(self.minstack)
+        return (self.minstack[-1])
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
